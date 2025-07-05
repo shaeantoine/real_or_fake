8abeb55631994330d62ed6d7e4fdb8a19a45fb56
shaeantoine <shaeantoine@gmail.com>
2025-07-05 23:42:23 +1000

    Fix data directory variable in loader

--- dataloader.py
+++ dataloader.py
@@ -6,17 +6,17 @@ def extract_data(data_dir):
         # Read in training data labels
         df_labels = pd.read_csv(train_labels)["real_text_id"]
 
-    # Iterate through training data directories and add text files + label to array
+    # Iterate through training data directories and add text files (+ label) to array
     data = []
-    i = 0
-    for dirpath, _, filenames in sorted(os.walk(train_dir)):
+    i = 1
+    for dirpath, _, filenames in sorted(os.walk(data_dir)):
         try: 
             with open(os.path.join(dirpath, filenames[0]), 'r', encoding='utf-8') as f1:
                 text1 = f1.read().strip()
             with open(os.path.join(dirpath, filenames[1]), 'r', encoding='utf-8') as f2:
                 text2 = f2.read().strip()
 
-            # Appending text from file 1 and file 2 as well as real data label
+            # Appending text from file 1 and file 2 (+ real data label if applicable)
             if "train" in data_dir:
                 data.append((text1, text2, df_labels[i]))
             else:
