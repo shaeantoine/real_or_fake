e3be7e5f484afd05dadd8e52311e8f2debaad11d
shaeantoine <shaeantoine@gmail.com>
2025-07-05 22:28:43 +1000

    Remove data ingestion code from training loop file

--- siamesetrain.py
+++ siamesetrain.py
@@ -11,39 +11,6 @@ from sklearn.metrics import accuracy_score
 from siamesenetwork import SiameseNetwork
 from textpairdataset import TextPairDataset
 
-
-# ====================================
-# Data ingestion 
-# ====================================
-train_dir = "data/train/"
-train_labels = "data/train.csv"
-
-# Reading in labels 
-df = pd.read_csv(train_labels)
-df_labels = df["real_text_id"]
-
-# Reading in and appending text data + label to data[]
-i = 0 
-data = []
-for dirpath, dirnames, filenames in sorted(os.walk(train_dir)): 
-    try:
-        with open(os.path.join(dirpath, filenames[0]), 'r', encoding='utf-8') as f1:
-            text1 = f1.read().strip()
-        with open(os.path.join(dirpath, filenames[1]), 'r', encoding='utf-8') as f2:
-            text2 = f2.read().strip()
-
-        # Appending text from file 1 and file 2 as well as real data label
-        data.append((text1, text2, df_labels[i]))
-    except:
-        continue 
-
-    i += 1
-
-# Converting data[] to a Pandas DF
-train_df = pd.DataFrame(data, columns=['file_1', 'file_2', "real_file_label"])
-train_df.head()
-
-
 # ====================================
 # Training Loop
 # ====================================
