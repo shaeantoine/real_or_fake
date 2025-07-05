14cfc5e97355abecb9b9696e51a0b7b5f0ff5349
shaeantoine <shaeantoine@gmail.com>
2025-07-05 22:25:40 +1000

    Moving data extraction to separate file

--- dataloader.py
+++ dataloader.py
@@ -1,32 +1,46 @@
 import os 
 import pandas as pd
 
+def extract_data(data_dir): 
+    if "train" in data_dir:
+        # Read in training data labels
+        df_labels = pd.read_csv(train_labels)["real_text_id"]
+
+    # Iterate through training data directories and add text files + label to array
+    data = []
+    i = 0
+    for dirpath, _, filenames in sorted(os.walk(train_dir)):
+        try: 
+            with open(os.path.join(dirpath, filenames[0]), 'r', encoding='utf-8') as f1:
+                text1 = f1.read().strip()
+            with open(os.path.join(dirpath, filenames[1]), 'r', encoding='utf-8') as f2:
+                text2 = f2.read().strip()
+
+            # Appending text from file 1 and file 2 as well as real data label
+            if "train" in data_dir:
+                data.append((text1, text2, df_labels[i]))
+            else:
+                data.append((text1, text2))
+
+        except: 
+            continue 
+
+        i += 1
+    
+    # Converting data[] to a Pandas DF
+    # Storing new dataset as CSV
+    if "train" in data_dir:
+        df = pd.DataFrame(data, columns=['file_1', 'file_2', "real_file_label"]) 
+        df.to_csv("data/train_df.csv")
+    else:
+        df = pd.DataFrame(data, columns=['file_1', 'file_2']) 
+        df.to_csv("data/test_df.csv")
+
+
+# Extracting 
 train_dir = "data/train/"
 train_labels = "data/train.csv"
+test_dir = "data/test/"
 
-# Read in training data labels
-df = pd.read_csv(train_labels)
-df_labels = df["real_text_id"]
-
-# Iterate through training data directories and add text files + label to array
-data = []
-i = 0
-for dirpath, dirnames, filenames in sorted(os.walk(train_dir)):
-    try: 
-        with open(os.path.join(dirpath, filenames[0]), 'r', encoding='utf-8') as f1:
-            text1 = f1.read().strip()
-        with open(os.path.join(dirpath, filenames[1]), 'r', encoding='utf-8') as f2:
-            text2 = f2.read().strip()
-
-        # Appending text from file 1 and file 2 as well as real data label
-        data.append((text1, text2, df_labels[i]))
-    except: 
-        continue 
-
-    i += 1
-
-# Converting data[] to a Pandas DF
-train_df = pd.DataFrame(data, columns=['file_1', 'file_2', "real_file_label"])
-
-# Storing new dataset as CSV
-train_df.to_csv("train_df.csv") 
+extract_data(train_dir)
+extract_data(test_dir)
